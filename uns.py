b02709dea1857c179820755a04af2937fa2653ba
Allex Radu <eusceptic@gmail.com>
2020-09-24 11:46:48 +0300

    updated code

--- uns.py
+++ uns.py
@@ -8,30 +8,30 @@ import excel
 # a = products[b'code'].decode('utf-8')
 # print(a)
 
-# product_codes = []
-#
-# products = excel.get_all_the_rows_from_column('J')
-#
-# for prod in products:
-#     if prod is not None:
-#         dictionary = loads(loads(serialize(prod)))
-#         p_code = dictionary[b'code']
-#         if p_code is not None:
-#             product_codes.append(p_code.decode('utf-8'))
-#         else:
-#             product_codes.append('n/a')
-#     else:
-#         product_codes.append('n/a')
-#
-# excel.write_product_code_to_excel(product_codes, 'K')
+product_codes = []
 
-key_columns = ['G', 'K', 'O', 'S', 'W', 'AA', 'AE', 'AI', 'AM', 'AQ', 'AU', 'AY', 'BC', 'BG']
-value_columns = ['I', 'M', 'Q', 'U', 'Y', 'AC', 'AG', 'AK', 'AO', 'AS', 'AW', 'BA', 'BE', 'BI']
+products = excel.get_all_the_rows_from_column('AL')
 
-# key_columns = ['G', 'K']
-# value_columns = ['I', 'M']
+for prod in products:
+    if prod is not None:
+        dictionary = loads(loads(serialize(prod)))
+        p_code = dictionary[b'code']
+        if p_code is not None:
+            product_codes.append(p_code.decode('utf-8'))
+        else:
+            product_codes.append('n/a')
+    else:
+        product_codes.append('n/a')
 
-for i in range(len(key_columns)):
-    cell_index = excel.match_key_value(key_columns[i], value_columns[i])
+excel.write_product_code_to_excel(product_codes, 'AM')
+
+# key_columns = ['I', 'K', 'M', 'O', 'Q', 'S', 'U', 'W', 'Y', 'AA', 'AC']
+# value_columns = ['J', 'L', 'N', 'P', 'R', 'T', 'V', 'X', 'Z', 'AB', 'AD']
+#
+# # key_columns = ['G', 'K']
+# # value_columns = ['I', 'M']
+#
+# for i in range(len(key_columns)):
+#     cell_index = excel.match_key_value(key_columns[i], value_columns[i])
 
 # excel.match_key_value(key_columns[0], value_columns[0])
